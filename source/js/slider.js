3f6e06f1eaf632a1338fda48864a77eee5021bfd
tis <tisbootis@gmail.com>
2020-08-20 17:35:37 +0300

    add rehabs-swiper block

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,4 +1,4 @@
-// слайдер рехабов
+// слайдер врачей
 $(document).ready(function () {
 
   let doctorsSwiper = new Swiper('#doctors-swiper', {
@@ -16,6 +16,34 @@ $(document).ready(function () {
     breakpoints: {
       756: {
         slidesPerView: '3',
+        spaceBetween: 20,
+      },
+      // 550: {
+      //   slidesPerView: '3',
+      //   spaceBetween: 30,
+      // }
+    }
+  });
+});
+
+// слайдер клиник
+$(document).ready(function () {
+
+  let rehabsSwiper = new Swiper('#rehabs-swiper', {
+    slidesPerView: '2',
+    spaceBetween: 25,
+    touchRatio: 1,
+
+    watchSlidesVisibility: true, // удаляем тень у слайдов вне поля видимости
+
+    navigation: {
+      nextEl: '.rehabs-swiper__button-next',
+      prevEl: '.rehabs-swiper__button-prev',
+    },
+
+    breakpoints: {
+      756: {
+        slidesPerView: '4',
         spaceBetween: 20,
       },
       // 550: {
